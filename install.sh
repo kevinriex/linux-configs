889324d252fb2bb2f6d677a395d86f1d29d5bf9d
Kevin Riexinger <55785977+kevinriex@users.noreply.github.com>
2021-11-27 17:13:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-sudo apt-get install coreutils sed git build-essential gcc-11 g++-11 apache2 libapache2-mod-php php php-mysql mysql-server 
+sudo apt-get install coreutils sed git build-essential gcc-11 g++-11 apache2 libapache2-mod-php php php-mysql mysql-server gnupg 
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+git clone https://github.com/aristocratos/btop.git
+cd btop
+make
+sudo make install
+sudo make setuid
+cd ..
